dd0af9f77aad8ef732c2286e75829e9b89f04552
macszl <235989@edu.p.lodz.pl>
2023-04-26 14:49:54 +0200

    added logout

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,38 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
+  if (!authed) {
+    res.render("index");
+  } else {
+    var oauth2 = google.oauth2({
+      auth: oAuth2Client,
+      version: "v2",
+    });
+    oauth2.userinfo.get(function (err, response) {
+      if (err) {
+        console.log("Error receiving response.");
+        console.log(err);
+      } else {
+        console.log("Response data.");
+        console.log(response.data);
+        res.render("loggedin", { title: "Express", user: response.data });
+      }
+    });
+  }
+});
+
+router.get("/login", function (req, res, next) {
+  console.log("Authed: " + authed);
+  console.log("Oauth2Client: " + oAuth2Client);
+  console.log("Google: " + google);
   try {
     if (!authed) {
       const url = oAuth2Client.generateAuthUrl({
         access_type: "offline",
         scope: "https://www.googleapis.com/auth/userinfo.profile",
       });
-      console.log("Url problems.");
+      console.log("Url received.");
       console.log(url);
       res.redirect(url);
     } else {
@@ -19,16 +43,28 @@ router.get("/", function (req, res, next) {
       });
       oauth2.userinfo.get(function (err, response) {
         if (err) {
-          console.log("Error.");
+          console.log("Error receiving response.");
           console.log(err);
         } else {
           console.log("Response data.");
           console.log(response.data);
-          res.render("index", { title: "Express", user: response.data });
+          res.render("loggedin", { title: "Express", user: response.data });
         }
       });
     }
   } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/logout", function (req, res) {
+  try {
+    console.log("logging out");
+    authed = false;
+    oAuth2Client.setCredentials(null);
+    res.redirect("/");
+  } catch (err) {
+    console.log("logout failed");
     console.log(err);
   }
 });
@@ -46,7 +82,7 @@ router.get("/auth/google/callback", function (req, res) {
           console.log(tokens);
           oAuth2Client.setCredentials(tokens);
           authed = true;
-          res.redirect("/");
+          res.redirect("/login");
         }
       });
     }
